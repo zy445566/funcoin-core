839a7b246927f1fa7d48013b9a00a4bc1a102b86
Zhang Yuan <zhangyuan@ZhangdeMacBook-Air.local>
2018-01-13 19:48:44 +0800

    add Block

--- config.js
+++ config.js
@@ -1,7 +1,16 @@
+const path = require("path");
 module.exports = {
     godBlock:{
-        parentBlock:null,
-        prompt:"Funcoin will be true",
-        difficulty:2
-    }
+        id:"fc1024fc",
+        parentId:null,
+        timestamp:new Date().getTime(),
+        parentTimestamp:new Date().getTime(),
+        vail:{
+            difficulty:1,
+            random:Math.floor(Math.random()*16).toString(16),
+        },
+        result:null
+    },
+    outPutBlockTime:10*60*1000,
+    dataPath:path.join(__dirname,"data")
 };
