ab7cffb87371f5de9bfebd58a43cd8b775c6b0a8
ZhangYuan <zyct@vip.qq.com>
2018-02-08 15:03:01 +0800

    update block

--- config.js
+++ config.js
@@ -3,18 +3,6 @@ module.exports = {
     rpc:{
         port:52220
     },
-    godBlock:{
-        id:"fc1024fc",
-        parentId:null,
-        timestamp:new Date().getTime(),
-        parentTimestamp:new Date().getTime(),
-        vail:{
-            difficulty:1,
-            random:Math.floor(Math.random()*16).toString(16),
-        },
-        result:null,
-        producerAddress:""
-    },
     awardNum:25,
     outPutBlockTime:10*60*1000,
     dataPath:path.join(__dirname,"data")
