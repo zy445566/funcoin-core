bec87793140808d7b6330792db1638238acbca95
ZhangYuan <zyct@vip.qq.com>
2018-02-23 16:35:56 +0800

    update

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ module.exports = {
         port:52220
     },
     awardNum:25,
+    totalNum:2100*10000,
+    releaseNum:0,//根据挖矿难度计算释放金额的回归年份
     outPutBlockTime:10*60*1000,
     dataPath:path.join(__dirname,"data")
 };
