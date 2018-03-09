a74bfbfb6e2af9b804ab3843f4f0e8ec645e157c
ZhangYuan <zyct@vip.qq.com>
2018-03-09 15:10:27 +0800

    modify

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 const path = require("path");
 module.exports = {
-    rpc:{
-        port:52220
-    },
     awardNum:25,
     totalNum:2100*10000,
-    releaseNum:0,//根据挖矿难度计算释放金额的回归年份
+    costRate:0.002,
+    baseMergeTime:18*30*24*3600*1000,
+    baseReturnTime:50*18*30*24*3600*1000,
     outPutBlockTime:10*60*1000,
     dataPath:path.join(__dirname,"data")
 };
