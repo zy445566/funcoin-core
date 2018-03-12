f268f7f17edb12fadbb360a2641a9c0d15abd750
ZhangYuan <zyct@vip.qq.com>
2018-03-12 14:09:37 +0800

    update

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
     awardNum:25,
     totalNum:2100*10000,
     costRate:0.002,
+    baseUnit:100000000,
     baseMergeTime:18*30*24*3600*1000,
     baseReturnTime:50*18*30*24*3600*1000,
     outPutBlockTime:10*60*1000,
